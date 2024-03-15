5d38b713cbf32aad2a9c5f7d2b900b3885f8a611
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-03-15 07:23:06 -0400

    Show/Hide Add Subtopic Button
    - When there are no topics the add subtopic button will be hidden
    - When user adds a topic the add subtopic button will be displayed

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -1,12 +1,19 @@
 // This handles all the drop down menus and start / stop buttons
 const topicSelect_el = document.getElementById('topicSelect');
 
+const subtopicSelect_el = document.getElementById('subtopicSelect');
+const addSubtopicButton_el = document.getElementById('addSubtopicButton');
+
 async function populateTopicSelect(){
     topicSelect_el.innerHTML = '';
-    activeTopics.forEach(element => {
-        const topicOption_el = document.createElement('option');
-        topicOption_el.textContent = element.topic;
-
-        topicSelect_el.append(topicOption_el);
-    });
+    if (activeTopics.length > 0){
+        addSubtopicButton_el.style.visibility = 'visible';
+        activeTopics.forEach(element => {
+            const topicOption_el = document.createElement('option');
+            topicOption_el.textContent = element.topic;
+            topicSelect_el.append(topicOption_el);
+        });    
+    } else {
+        addSubtopicButton_el.style.visibility = 'hidden';
+    }
 }
