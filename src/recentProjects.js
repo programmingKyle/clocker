4cb827f8dbab2ed6ecff4a4a8bc70ab4c2599f50
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-03-18 04:56:03 -0400

    Populate Projects Div
    - For now there is no limit on how many projects it return
    - Need to change the database table so that it includes Topic and Subtopic info...this way it will display projects with the same name but under different topics/subtopics

--- src/recentProjects.js
+++ src/recentProjects.js
@@ -1,3 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-})
+const projectListDiv_el = document.getElementById('projectListDiv');
+
+function populateRecentProjects(){
+    console.log(activeProjects);
+
+    activeProjects.forEach(element => {
+        const projectItem_el = document.createElement('div');
+        projectItem_el.classList.add('project-item-div');
+
+        const projectName_el = document.createElement('p');
+        projectName_el.textContent = element.project;
+
+        const projectTimeText_el = document.createElement('p');
+        projectTimeText_el.textContent = 'NA YET';
+
+        const projectTopicText_el = document.createElement('h5');
+        projectTopicText_el.textContent = 'NA YET';
+
+        const projectSubtopicText_el = document.createElement('h5');
+        projectSubtopicText_el.textContent = 'NA YET';
+
+        projectItem_el.append(projectName_el, projectTimeText_el, projectTopicText_el, projectSubtopicText_el);
+
+        projectListDiv_el.append(projectItem_el);
+    });
+}
