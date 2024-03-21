0367567b726672e8e263afb906c9eb13fbbce7e7
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-03-21 11:28:44 -0400

    Subtopic View Back Button
    - Click on a topic to get into the subtopic view there is now a button that will get you back to topic view

--- src/subtopicView.js
+++ src/subtopicView.js
@@ -1,8 +1,13 @@
 // Will be used when user clicks on a topic to review its information and display subtopics
 // topicListDiv_el
+const subtopicViewBackButton_el = document.getElementById('subtopicViewBackButton');
 
 async function populateSubtopics(topic){
     console.log(topic);
     topicSubtopicHeader_el.textContent = 'Subtopics';
     topicListDiv_el.innerHTML = '';
 }
+
+subtopicViewBackButton_el.addEventListener('click', async () => {
+    await populateTopicView();
+});
