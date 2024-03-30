59235ecab9401990b5f30e9f1207efeaf4f556fe
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-03-30 15:52:08 -0400

    Options Overlay Close Button
    - User can use the x button to close the options overlay
    - Added some CSS to the progress bar section

--- src/optionsHandler.js
+++ src/optionsHandler.js
@@ -7,4 +7,13 @@ optionsButton_el.addEventListener('click', () => {
 
 async function optionsListeners(){
     console.log('Listeners');
+    const optionsCloseButton_el = document.getElementById('optionsCloseButton');
+    const progressBarSelect_el = document.getElementById('progressBarSelect');
+    const progressBarHour_el = document.getElementById('progressBarHour');
+    const progressBarMinute_el = document.getElementById('progressBarMinute');
+    const saveOptionsButton_el = document.getElementById('saveOptionsButton');
+
+    optionsCloseButton_el.addEventListener('click', () => {
+        overlayContainer_el.style.display = 'none';
+    });
 }
